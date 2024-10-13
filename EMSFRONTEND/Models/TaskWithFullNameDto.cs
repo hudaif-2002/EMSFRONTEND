f093bd49254cb1e28c42ee5a3c9492b367bb5a54
dinujoy-tech <dinujoy01@gmail.com>
2024-10-13 17:02:12 +0530

    Updated EmpTaskList controller

---
+++ EMSFRONTEND/Models/TaskWithFullNameDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EMSFRONTEND.Models
+{
+    public class TaskWithFullNameDto
+    {
+        public int TaskId { get; set; }
+
+        public string Title { get; set; }
+
+        public int UserId { get; set; }
+
+        public string FullName { get; set; }  // FullName added here
+
+        public DateTime AssignedDate { get; set; }
+
+        public DateTime DeadlineDate { get; set; }
+
+        public string Description { get; set; }
+
+        public string TaskStatus { get; set; }
+
+        public string Resources { get; set; }
+    }
+}
